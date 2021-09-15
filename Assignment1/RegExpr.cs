84cea123854761abe23f94b77b03e64743035ce1
Frederik Avlund <favl@itu.dk>
2021-09-15 12:24:32 +0200

    Merged Regex Exercises together

--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -6,9 +6,20 @@ namespace Assignment1
 {
     public static class RegExpr
     {
-        public static IEnumerable<string> SplitLine(IEnumerable<string> lines)
+
+        static void Main(string[] args)
         {
-            throw new NotImplementedException();
+        }
+        
+       public static IEnumerable<string> SplitLine(IEnumerable<string> lines)
+        {
+            string strRegex = "[^a-zA-Z0-9]";
+            foreach (string s in lines) {
+                foreach (var match in Regex.Split(s, strRegex))
+                {
+                    yield return match;
+                }
+            }
         }
 
         public static IEnumerable<(int width, int height)> Resolution(string resolutions)
@@ -19,8 +30,8 @@ namespace Assignment1
             {
                 yield return (Int32.Parse(match.Groups["firstYear"].Value), Int32.Parse(match.Groups["secondYear"].Value));
             }
-
         }
+
         public static IEnumerable<string> InnerText(string html, string tag)
         {
             string pattern = @$"<{tag}.*?>(.*?)<\/{tag}>*";
